d807c8f9d87d9f623b29699679cf1e593d881b5a
Klodian Behrami <klodianbehrami@gmail.com>
2023-10-02 12:17:14 -0400

    added csrf and express error handling

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,67 @@
-
 const express = require("express");
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const debug = require('debug');
 
-const usersRouter = require('./routes/api/users'); // update the import file path
+const cors = require('cors');
+const csurf = require('csurf');
+const { isProduction } = require('./config/keys');
+
+const usersRouter = require('./routes/api/users');
+const csrfRouter = require('./routes/api/csrf');
 
 const app = express();
 
-app.use(logger('dev')); // log request components (URL/method) to terminal
-app.use(express.json()); // parse JSON request body
-app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
-app.use(cookieParser()); // parse cookies as an object on req.cookies
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Security Middleware
+if (!isProduction) {
+  // Enable CORS only in development because React will be on the React
+  // development server (http://localhost:3000). (In production, React files
+  // will be served statically on the Express server.)
+  app.use(cors());
+}
+
+// Set the _csrf token and create req.csrfToken method to generate a hashed
+// CSRF token
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+);
 
 // Attach Express routers
-app.use('/api/users', usersRouter); // update the path
+app.use('/api/users', usersRouter);
+app.use('/api/csrf', csrfRouter);
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+  });
+  
+  const serverErrorLogger = debug('backend:error');
+  
+  // Express custom error handler that will be called whenever a route handler or
+  // middleware throws an error or invokes the `next` function with a truthy value
+  app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+      message: err.message,
+      statusCode,
+      errors: err.errors
+    })
+});
+  
+
 
 module.exports = app;
