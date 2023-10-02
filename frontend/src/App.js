f0b693e3f0c769180ec593ea7f9995f69b9b4fc1
Sean <pca.sean.jeun@gmail.com>
2023-10-02 11:46:05 -0400

    setup git repo fix

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import { Switch } from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
+import NavBar from './components/NavBar/NavBar';
+
+import MainPage from './components/MainPage/MainPage';
+import LoginForm from './components/SessionForms/LoginForm';
+import SignupForm from './components/SessionForms/SignupForm';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Switch>
+        <AuthRoute exact path="/" component={MainPage} />
+        <AuthRoute exact path="/login" component={LoginForm} />
+        <AuthRoute exact path="/signup" component={SignupForm} />
+      </Switch>
+    </>
+  );
+}
+
+// comment
+
+export default App;
---
+++ frontend/src/components/NavBar/NavBar.js
@@ -0,0 +1,42 @@
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import './NavBar.css';
+import { logout } from '../../store/session';
+
+function NavBar() {
+    const loggedIn = useSelector(state => !!state.session.user);
+    const dispatch = useDispatch();
+
+    const logoutUser = e => {
+        e.preventDefault();
+        dispatch(logout());
+    }
+
+    const getLinks = () => {
+        if (loggedIn) {
+            return (
+                <div className="links-nav">
+                    <Link to={'/signin'}>Signin</Link>
+                    <Link to={'/signup'}>Signup</Link>
+                    <button onClick={logoutUser}>Logout</button>
+                </div>
+            );
+        } else {
+            return (
+                <div className="links-auth">
+                    <Link to={'/signup'}>Signup</Link>
+                    <Link to={'/login'}>Login</Link>
+                </div>
+            );
+        }
+    }
+
+    return (
+        <>
+            <h1>TuneUp</h1>
+            {getLinks()}
+        </>
+    );
+}
+
+export default NavBar;
