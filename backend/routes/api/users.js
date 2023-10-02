2eeaceffa653f1a6a8bcf3b97ebea73873f24273
Klodian Behrami <klodianbehrami@gmail.com>
2023-10-02 15:11:39 -0400

    added validations

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -6,6 +6,8 @@ const User = mongoose.model('User');
 const passport = require('passport');
 const { loginUser, restoreUser } = require('../../config/passport');
 const { isProduction } = require('../../config/keys');
+const validateRegisterInput = require('../../validations/register');
+const validateLoginInput = require('../../validations/login');
 
 router.get('/current', restoreUser, (req, res) => {
   if (!isProduction) {
@@ -29,7 +31,7 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.post('/register', async (req, res, next) => {
+router.post('/register', validateRegisterInput, async (req, res, next) => {
   // Check to make sure no one has already registered with the proposed email or
   // username.
   const user = await User.findOne({ email: req.body.email });
@@ -72,7 +74,7 @@ router.post('/register', async (req, res, next) => {
   });
 });
 
-router.post('/login', async (req, res, next) => {
+router.post('/login', validateLoginInput, async (req, res, next) => {
   passport.authenticate('local', async function(err, user) {
     if (err) return next(err);
     if (!user) {
