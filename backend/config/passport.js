e24920147f354c103726aea86322c1c228db29d5
Klodian Behrami <klodianbehrami@gmail.com>
2023-10-02 14:27:59 -0400

    adding jwt

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -5,6 +5,35 @@ const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const jwt = require('jsonwebtoken');
 const { secretOrKey } = require('./keys');
+const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
+const options = {};
+options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+options.secretOrKey = secretOrKey;
+
+passport.use(new JwtStrategy(options, async (jwtPayload, done) => {
+  try {
+    const user = await User.findById(jwtPayload._id)
+    if (user) {
+      // return the user to the frontend
+      return done(null, user);
+    }
+    // return false since there is no user
+    return done(null, false);
+  }
+  catch(err) {
+    done(err);
+  }
+}));
+
+exports.requireUser = passport.authenticate('jwt', { session: false });
+
+exports.restoreUser = (req, res, next) => {
+    return passport.authenticate('jwt', { session: false }, function(err, user) {
+      if (err) return next(err);
+      if (user) req.user = user;
+      next();
+    })(req, res, next);
+};
 
 passport.use(new LocalStrategy({
     session: false,
