ba1a0f3ac103a70c9f4a772fa71fc2e63ad2d4d1
Dennis Lee <dennislee1499@gmail.com>
2023-10-03 11:19:43 -0400

    styled homepage/added slogan

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -1,9 +1,38 @@
-function homepage() {
-    return (
-        <>
-        
-        </>
-    )
+import { useSelector } from "react-redux";
+import homepagebackground from "./home-page-background.png"
+import "./HomePage.css"
+
+function HomePage() {
+  const currentUser = useSelector((state) => state.session.user);
+
+  return (
+    <div className="main-container">
+      <div className="greeting-container">
+        {currentUser && (
+            <h1>
+                Hey {currentUser.firstName} {currentUser.lastName}!
+            </h1>
+        )}
+      </div>
+      <div className="slogan-container">
+        <div class="slogan">Find your band's missing beat</div>
+        <div class="slogan">Discover the Rhythm of Friendship</div>
+        <div class="slogan">Less Lonely, More Melody!</div>
+      </div>
+      <footer>
+        <div className="scrolling-text-homepage">
+          Copyright &copy; 2023 TuneUp | Developed by: Dan L. Klodian B.
+          Mudassar M. Dennis L. Sean J.
+        </div>
+      </footer>
+      <img
+        className="home-page-background"
+        src={homepagebackground}
+        alt="Background"
+      />
+    </div>
+  );
 }
 
-export default homepage;
+
+export default HomePage;
