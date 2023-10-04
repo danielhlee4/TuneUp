7997c0d30599734f4d071138a28e645305bd18bd
Sean <pca.sean.jeun@gmail.com>
2023-10-03 20:54:22 -0400

    update form/function built. Need testing

---
+++ frontend/src/store/users.js
@@ -0,0 +1,30 @@
+import jwtFetch from "./jwt";
+
+const RECEIVE_USER = "users/RECEIVE_USER"
+
+const receiveUser = (user) => ({
+    type: RECEIVE_USER,
+    user
+})
+
+export const update = (user) => updateUser(user, `api/users/:id`)
+
+export const updateUser = (newUserInfo, route) => async dispatch => {
+    const res = await jwtFetch(route, {
+        method: "PATCH",
+        body: JSON.stringify(newUserInfo),
+    });
+    if (res.ok) {
+        const newInfo = await res.json()
+        dispatch(receiveUser(newInfo))
+    }
+}
+
+export const usersReducer = (state = {}, action) => {
+    Object.freeze(state)
+    const nextState = { ...state }
+    switch(action.type) {
+        case RECEIVE_USER:
+            return { ...nextState, [action.user.id]: action.user }
+    }
+}
