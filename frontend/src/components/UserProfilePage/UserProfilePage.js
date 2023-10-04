4c032cce19621379ca8a98e7a1a61d9b28bb5f83
Sean <pca.sean.jeun@gmail.com>
2023-10-04 14:33:10 -0400

    jwtfetch change

--- frontend/src/components/UserProfilePage/UserProfilePage.js
+++ frontend/src/components/UserProfilePage/UserProfilePage.js
@@ -1,10 +1,11 @@
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { getUser } from "../../store/users";
 
 function UserProfilePage() {
     const currentUser = useSelector((state) => state.session.user);
     const { id } = useParams()
-    const user = useSelector((state) =>  state.users[id]) 
+    const user = useSelector(getUser(id)) 
     console.log(user)
     return (
         <div>
