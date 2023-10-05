cca8471659e254eb2e0edb12c2a30f0b5877afb2
Dennis Lee <dennislee1499@gmail.com>
2023-10-04 21:13:17 -0400

    search actions/reducers

--- frontend/src/store/search.js
+++ frontend/src/store/search.js
@@ -1,39 +1,41 @@
 import jwtFetch from "./jwt";
 
+
 const RECEIVE_SEARCH_RESULTS = "search/RECEIVE_SEARCH_RESULTS";
 const CLEAR_SEARCH_RESULTS = "search/CLEAR_SEARCH_RESULTS";
 const RECEIVE_SEARCH_ERRORS = "search/RECEIVE_SEARCH_ERRORS";
 const CLEAR_SEARCH_ERRORS = "search/CLEAR_SEARCH_ERRORS";
 
 
-export const receiveSearchResults = (users) => ({
-    type: RECEIVE_SEARCH_RESULTS,
-    users,
+export const receiveSearchResults = (results) => ({
+  type: RECEIVE_SEARCH_RESULTS,
+  results,
 });
 
 export const receiveSearchErrors = (errors) => ({
-    type: RECEIVE_SEARCH_ERRORS,
-    errors, 
+  type: RECEIVE_SEARCH_ERRORS,
+  errors,
 });
 
 export const clearSearchResults = () => ({
-    type: CLEAR_SEARCH_RESULTS,
+  type: CLEAR_SEARCH_RESULTS,
 });
 
 export const clearSearchErrors = () => ({
-    type: CLEAR_SEARCH_ERRORS,
+  type: CLEAR_SEARCH_ERRORS,
 });
 
-export const searchUsers = (query) => async (dispatch) => {
+
+
+export const search = (criteria) => async (dispatch) => {
+  const queryString = new URLSearchParams(criteria).toString();
   try {
-    const response = await jwtFetch(
-      `/api/users/search?query=${encodeURIComponent(query)}`
-    );
+    const response = await jwtFetch(`/api/search?${queryString}`);
     const data = await response.json();
     if (data.error) {
       dispatch(receiveSearchErrors(data.error));
     } else {
-      dispatch(receiveSearchResults(data.users));
+      dispatch(receiveSearchResults(data.users)); 
     }
   } catch (error) {
     dispatch(receiveSearchErrors("Failed to fetch search results."));
@@ -42,32 +44,35 @@ export const searchUsers = (query) => async (dispatch) => {
 };
 
 
-const initialState = {
-    users: [],
-    errors: null,
+
+const initialSearchState = {
+  results: [],
+  errors: null,
 };
 
-const searchReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case RECEIVE_SEARCH_RESULTS:
-            return {
-                ...state,
-                users: action.users,
-                errors: null,
-            };
-        case RECEIVE_SEARCH_ERRORS:
-            return {
-                ...state,
-                errors: action.errors,
-            };
-        case CLEAR_SEARCH_RESULTS:
-            return initialState;
-        default:
-            return state;
-    }
+const searchReducer = (state = initialSearchState, action) => {
+  switch (action.type) {
+    case RECEIVE_SEARCH_RESULTS:
+      return {
+        ...state,
+        results: action.results,
+        errors: null,
+      };
+    case RECEIVE_SEARCH_ERRORS:
+      return {
+        ...state,
+        errors: action.errors,
+      };
+    case CLEAR_SEARCH_RESULTS:
+      return initialSearchState;
+    default:
+      return state;
+  }
 };
 
-const nullSearchErrors = null; 
+
+
+const nullSearchErrors = null;
 
 export const searchErrorsReducer = (state = nullSearchErrors, action) => {
   switch (action.type) {
@@ -83,3 +88,4 @@ export const searchErrorsReducer = (state = nullSearchErrors, action) => {
 
 
 export default searchReducer;
+
