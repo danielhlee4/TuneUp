f47c05b57436ccd1c617fe9f39d72ac97074b150
Dan Lee <danielhlee4@gmail.com>
2023-10-08 22:20:29 -0400

    add discover page map correspondance to search

--- frontend/src/components/Map/MultipleZipcodeMap.js
+++ frontend/src/components/Map/MultipleZipcodeMap.js
@@ -44,7 +44,13 @@ function MultipleZipcodeMap({ zipcodes }) {
             });
           });
   
-          createdMap.fitBounds(bounds);
+          // Check if there's only one zipcode
+          if (zipcodes.length === 1) {
+            createdMap.setCenter(coordinatesArray[0]);
+            createdMap.setZoom(14); // Set your desired zoom level for a single zipcode
+          } else {
+              createdMap.fitBounds(bounds);
+          }
   
           setMap(createdMap);
         });
