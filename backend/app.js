e470d7b9a3e654cfe2dd76b8a5cf251fec0b31e0
Klodian Behrami <klodianbehrami@gmail.com>
2023-10-10 13:54:26 -0400

    fixing stuff for render

--- backend/app.js
+++ backend/app.js
@@ -53,6 +53,28 @@ app.use("/api/tuneups", restoreUser, tuneUpRouter);
 app.use("/api/distance", distanceRoutes);
 app.use("/api/search", searchRouter);
 
+if (isProduction) {
+  const path = require('path');
+  // Serve the frontend's index.html file at the root route
+  app.get('/', (req, res) => {
+    res.cookie('CSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../frontend', 'build', 'index.html')
+    );
+  });
+
+  // Serve the static assets in the frontend's build folder
+  app.use(express.static(path.resolve("../frontend/build")));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  app.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('CSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../frontend', 'build', 'index.html')
+    );
+  });
+}
+
 app.use((req, res, next) => {
   const err = new Error("Not Found");
   err.statusCode = 404;
