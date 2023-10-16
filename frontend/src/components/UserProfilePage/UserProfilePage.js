9bc78758856f9c8f03d3ce3eec688abeaacad734
Dennis Lee <dennislee1499@gmail.com>
2023-10-15 21:35:41 -0400

    styled user profile

--- frontend/src/components/UserProfilePage/UserProfilePage.js
+++ frontend/src/components/UserProfilePage/UserProfilePage.js
@@ -46,6 +46,14 @@ function UserProfilePage() {
                         <div className="user-email-container">
                             <div className="user-email">{user?.email}</div>
                         </div>
+                        <div className= "user-instruments-container"> 
+                              <h3 className="instruments-title">Instruments:</h3>
+                              <div className="user-instruments">{user?.instruments?.join(', ')}</div>
+                        </div>
+                        <div className="user-genres-container">
+                              <h3 className="genres-title">Favorite Genres:</h3>
+                              <div className="user-genres">{user?.genres?.join(', ')}</div>
+                        </div>
                         <div className="user-address-container">
                             <div className="user-address">{user?.address}</div>
                         </div>
